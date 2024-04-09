842bb9b73779249e9489d57542b9ab2e4337e2bd
Gastón E. Acosta <gasteac@gmail.com>
2024-04-08 23:52:57 -0300

    create head section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { About, Dashboard, Home, Projects, SignIn, SignUp } from "./pages";
+import { Header } from "./components/Header";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/*   si pongo una ruta mal me redirije a home con el *   */}
-        <Route path="*" element={<Home />} /> 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          {/*   si pongo una ruta mal me redirije a home con el *   */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
