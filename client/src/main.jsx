3585ac26a7bcac647213ad6f2a56bcdcf5685371
Gastón E. Acosta <gasteac@gmail.com>
2024-04-10 05:33:04 -0300

    Add redux-persist to client dependencies and update store configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import "./index.css";
 import { Provider } from "react-redux";
-import { store } from './redux/store.js';
+//como ahora usamos redux-persist, traemos tmb el persistor de la store
+import { store, persistor } from "./redux/store.js";
+//y tambien tenes que importar el gate de persist que cubre TODO
+import { PersistGate } from "redux-persist/integration/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {userSlice} from "./user/userSlice";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { userSlice } from "./user/userSlice";
+
+//de aca para abajo es todo para utilizar redux persist, una especie de local storage
+//nos guarda basicamente toda la store en memoria
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+//creamos un combinador de reducers para dsp pasarle a la store como 1 solo reducer
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
+});
+
+//creamos la configuracion del persist, la key seria con el nombre que se guarda en el navegador
+const persistConfig = {
+  key: "root",
+  storage, //storage es de la librería de redux persist
+  version: 1,
+};
+
+//y aca al persistReducer (que va a perseverar en el tiempo de ahi su nombre)
+//le pasamos lo q configuramos antes, la conf, y el rootReducer q combina todos los reducers
+const persistedReducer = persistReducer(
+  persistConfig,
+  rootReducer,
+);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+  reducer: persistedReducer, //aca le pasamos el reducer que engloba todos los reducers
+
+  //requerimos este middleware para prevenir errores utilizando react toolkit
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+//exportamos el persistor que contiene a la store.
+export const persistor = persistStore(store)
