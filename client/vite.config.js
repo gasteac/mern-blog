c7a759dd81592bc6086cf49e95213a9ae5033b82
Gastón E. Acosta <gasteac@gmail.com>
2024-04-11 18:18:02 -0300

    Update page title in index.html and add proxy configuration for /api requests in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
+      // haceme un proxy de todas las peticiones del cliente que comiencen con /api al servidor (api)
       "/api": {
+        ///url del servidor
         target: "http://localhost:3000/",
+        //quiero que sea solo http
         secure: false,
       },
     },
