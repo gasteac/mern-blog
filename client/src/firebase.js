b52de4830d32ae4df889801ae1b07fd3c81ee913
Gastón E. Acosta <gasteac@gmail.com>
2024-04-12 15:45:42 -0300

    Update Firebase configuration in firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,3 @@
-//No tenes porque saber esto, es todo traido de firebase
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,14 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  //aca le pones la API KEY DE FIREBASE, como es privada la ponemos en .env
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  //todo esto se deja igual como vino de fabrica
-  authDomain: "mern-blog-cd6ab.firebaseapp.com",
-  projectId: "mern-blog-cd6ab",
-  storageBucket: "mern-blog-cd6ab.appspot.com",
-  messagingSenderId: "646835583072",
-  appId: "1:646835583072:web:09cf7a265135c1e25a6b05",
+  authDomain: "facerook-gasteac.firebaseapp.com",
+  projectId: "facerook-gasteac",
+  storageBucket: "facerook-gasteac.appspot.com",
+  messagingSenderId: "333118237893",
+  appId: "1:333118237893:web:0f525b3b38e656cf88d4ac",
 };
 
 // Initialize Firebase
