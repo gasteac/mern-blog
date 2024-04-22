3073d00e45520a26f99ad890b55cbca5f04031e5
Gaston Acosta <gasteac@gmail.com>
2024-04-22 11:25:16 -0300

    improve design

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -12,18 +12,24 @@ export const PostCard = ({ post }) => {
           className="w-screen rounded-xl h-52 lg:w-96 lg:h-96 object-cover transition-all ease-in-out duration-150"
         />
 
-        <p className="line-clamp-1 font-bold text-white text-xl absolute bottom-8 p-4">
+        <p
+          style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+          className="line-clamp-1 font-bold text-white text-xl absolute bottom-8 p-4"
+        >
           {post.title}
         </p>
-        <p className=" text-white text-sm absolute line-clamp-1 bottom-2 p-4">
+        <p
+          style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+          className=" text-white text-sm absolute line-clamp-1 bottom-2 p-4"
+        >
           {post.content}
         </p>
 
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black opacity-90 rounded-3xl lg:group-hover:bg-black lg:group-hover:bg-opacity-40 " />
         <Button
           pill
-          color={'gray'}
-          className="absolute shadow-2xl top-[40%] left-[50%] -translate-x-[50%] -translate-y-[50%]  lg:top-[50px] lg:left-[-100px] lg:group-hover:scale-[1.5]  border-none  lg:group-hover:-translate-x-[50%] lg:group-hover:-translate-y-[50%] lg:group-hover:top-[50%] lg:group-hover:left-[50%] lg:transition-all lg:ease-out lg:duration-150 hover:!scale-[1.33]"
+          color={"gray"}
+          className="absolute dark:bg-gray-700 shadow-2xl top-[40%] left-[50%] -translate-x-[50%] -translate-y-[50%]  lg:top-[50px] lg:left-[-100px] lg:group-hover:scale-[1.5]  border-none  lg:group-hover:-translate-x-[50%] lg:group-hover:-translate-y-[50%] lg:group-hover:top-[50%] lg:group-hover:left-[50%] lg:transition-all lg:ease-out lg:duration-150 hover:!scale-[1.33]"
         >
           See more
         </Button>
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -62,25 +62,31 @@ export const PostPage = () => {
   return (
     <>
       <main className="p-3 flex flex-col max-w-3xl mx-auto mt-5">
-        <div className="flex flex-col gap-2 items-center align-middle justify-center">
-          <h1 className="text-3xl font-bold uppercase lg:text-4xl">
-            {post && post.title}
-          </h1>
-          <span className="mb-2">
-            Created: {post && new Date(post.updatedAt).toLocaleDateString()}
-          </span>
-          <Link to={`/search?category=${post && post.category}`}>
-            <Button color="gray" pill>
-              {post && post.category}
-            </Button>
-          </Link>
+        <div className="relative w-full h-96">
+          <div className="absolute inset-0 bg-gradient-to-t from-transparent via-transparent to-black opacity-90 rounded-3xl lg:group-hover:bg-black lg:group-hover:bg-opacity-40 " />
+
+          <img
+            src={post && post.image}
+            alt={post && post.title}
+            className="w-full h-full object-cover rounded-3xl shadow-md dark:shadow-none"
+          />
+          <div className="absolute top-0 left-0 w-full text-center p-4">
+            <h1
+              style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+              className="text-3xl dark:text-white text-white font-bold uppercase
+              lg:text-4xl drop-shadow-lg"
+            >
+              {post && post.title}
+            </h1>
+            <span
+              style={{ textShadow: "1px 1px 2px rgba(0, 0, 0, 1)" }}
+              className="dark:text-white text-white"
+            >
+              Created: {post && new Date(post.updatedAt).toLocaleDateString()}
+            </span>
+          </div>
         </div>
 
-        <img
-          src={post && post.image}
-          alt={post && post.title}
-          className="mt-5 p-3 max-h-full w-full object-cover self-center rounded-3xl shadow-md dark:shadow-none"
-        />
         <div className="mt-5 mb-5 w-[90%] self-center bg-gray-300 rounded-xl dark:bg-slate-800 p-6">
           <p>{post && post.content}</p>
         </div>
@@ -93,7 +99,9 @@ export const PostPage = () => {
           It may interest you
         </h1>
         <Link to={`/all-posts`}>
-          <span className="font-semibold italic text-gray-500 hover:underline">See all posts</span>
+          <span className="font-semibold italic text-gray-500 hover:underline">
+            See all posts
+          </span>
         </Link>
         {recentPosts && (
           <div className="mt-5 flex flex-wrap items-center justify-center">
